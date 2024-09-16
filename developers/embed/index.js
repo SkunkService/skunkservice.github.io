e60060e51e70ba43cc517a137550d2ac2d1020f9
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2024-09-16 09:14:23 -0300

    Update index.js

--- developers/embed/index.js
+++ developers/embed/index.js
@@ -6,15 +6,15 @@ window.addEventListener('offline', () => {
 
 // Content loading
 function showOfflineNotification() {
-    const contentElement = document.getElementById('content');
-    contentElement.innerHTML = `
+    const bodyElement = document.body;
+    bodyElement.innerHTML = `
         <h1 style="color: red; font-size: 24px; font-weight: bold;">Disconnected</h1>
         <p style="color: gray; font-size: 18px;">Check your Connection and Try Again.</p>
         <button onclick="restartWebsite()" style="background-color: #007bff; color: #fff; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">Restart Website</button>
     `;
 
-    contentElement.style.textAlign = 'center';
-    contentElement.style.padding = '20px';
+    bodyElement.style.textAlign = 'center';
+    bodyElement.style.padding = '20px';
 }
 
 // Restart website
