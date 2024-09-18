c686940d556ac1326a1e95f1d5e92ed6082b77d4
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2024-09-18 15:51:41 -0300

    Create devtools-detection.js

---
+++ api/devtools-detection.js
@@ -0,0 +1,23 @@
+(function() {
+    let isConsoleOpen = false;
+    const threshold = 160;
+
+    function detectConsole() {
+        console.log("[VIOLATION]: The DevTools you are trying to Open the Console. The DevTools using Console will violate the SkunkService's Promises of the Community Guidelines.");
+        const widthThreshold = window.outerWidth - window.innerWidth > threshold;
+        const heightThreshold = window.outerHeight - window.innerHeight > threshold;
+        if (widthThreshold || heightThreshold) {
+            if (!isConsoleOpen) {
+                isConsoleOpen = true;
+                document.body.innerHTML=`<div class="container"><h1>You are using the DevTools!</h1>
+                <p>The Website were Protected Because There's a DevTools Openned.</p>
+                <p>Please, Close your DevTools and Restart the Website if you continue.</p>
+                <button onclick="window.location.reload();">Restart Website</button></div>`;
+            }
+        } else {
+            isConsoleOpen = false;
+        }
+    }
+
+    setInterval(detectConsole, 2000); // Check every second
+})();
