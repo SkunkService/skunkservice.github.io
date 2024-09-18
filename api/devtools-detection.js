14c3b78a0e25306eaf2fc2d367361cdfbc33a4a8
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2024-09-18 15:56:12 -0300

    Update devtools-detection.js

--- api/devtools-detection.js
+++ api/devtools-detection.js
@@ -9,8 +9,8 @@
         if (widthThreshold || heightThreshold) {
             if (!isConsoleOpen) {
                 isConsoleOpen = true;
-                document.body.innerHTML=`<div class="container"><h1>You are using the DevTools!</h1>
-                <p>The Website were Protected Because There's a DevTools Openned.</p>
+                document.body.innerHTML=`<div class="container"><h1>You've been Protected of this Website.</h1>
+                <p>The Website were Protected Because's a DevTools Openned.</p>
                 <p>Please, Close your DevTools and Restart the Website if you continue.</p>
                 <button onclick="window.location.reload();">Restart Website</button></div>`;
             }
@@ -21,3 +21,11 @@
 
     setInterval(detectConsole, 2000); // Check every second
 })();
+
+/*
+This allows you to use the following script tag for integration:
+Old Example:
+<script src="https://cdn.jsdelivr.net/gh/SkunkService/skunkservice.github.io/api/devtools-detection.js"></script>
+Latest Example:
+<script src="https://cdn.jsdelivr.net/gh/SkunkService/skunkservice.github.io@main/api/devtools-detection.js"></script>
+*/
