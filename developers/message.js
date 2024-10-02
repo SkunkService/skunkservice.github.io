95348870c9bd507b7d85fcfc77591d324a7a979a
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2024-10-02 02:55:42 -0300

    Update message.js

--- developers/message.js
+++ developers/message.js
@@ -3,6 +3,7 @@ function showNotification(message) {
     const notificationElement = document.getElementById('notification'); // Reference to the notification div
     const msgElement = document.getElementById('msg'); // Reference to the message <p> element
     msgElement.innerText = message;
+    notificationEvent.style.display = "flex";
 
     // Show notification with opacity animation
     gsap.fromTo(notificationElement, { opacity: 0 }, { opacity: 1, duration: 0.5 });
@@ -10,13 +11,19 @@ function showNotification(message) {
     // Hide notification after a few seconds
     setTimeout(() => {
         gsap.to(notificationElement, { opacity: 0, duration: 0.5 });
+        setTimeout(() => {
+            notificationEvent.style.display = "none";
+        }, 500);
     }, 3000);
 }
 
 // Button click event to acknowledge the notification
 document.getElementById('ok-notification').addEventListener('click', () => {
     gsap.to(document.getElementById('notification'), { opacity: 0, duration: 0.5 });
+    setTimeout(() => {
+        notificationEvent.style.display = "none";
+    }, 500);
 });
 
 // Example usage of showNotification
-showNotification('This is a notification message!');
+showNotification('Hi, Welcome to the API');
