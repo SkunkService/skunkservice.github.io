e52fff03da293337284c5f2b25ba476e31207eb9
SkunkPlatform <167719548+SkunkPlatform@users.noreply.github.com>
2024-10-08 15:48:46 -0300

    Create all-modules.mjs

---
+++ api/scripts/all-modules.mjs
@@ -0,0 +1,28 @@
+// Importing all modules
+const fePromise = fetch("https://cdn.jsdelivr.net/gh/SkunkService/skunkservice.github.io/api/scripts/element-fetch.js")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.text(); // Get the script content as text
+  })
+  .then(scriptContent => {
+    eval(scriptContent); // Execute the script content
+  })
+  .catch(error => {
+    console.error("Fetch error:", error);
+  });
+
+const frPromise = fetch("https://cdn.jsdelivr.net/gh/SkunkService/skunkservice.github.io/api/scripts/file-review.js")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.text(); // Get the script content as text
+  })
+  .then(scriptContent => {
+    eval(scriptContent); // Execute the script content
+  })
+  .catch(error => {
+    console.error("Fetch error:", error);
+  });
